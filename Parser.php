1d9de436ac7b0802e7349a41a7e88d97988129a8
Oleg Prolubshchikov <oleg@Olegs-MacBook.local>
2016-12-02 18:58:07 +0300

    init

---
+++ Parser.php
@@ -0,0 +1,68 @@
+<?php
+
+
+class Parser
+{
+    public $htmlContent;
+
+    protected $url;
+
+    public function __construct($url)
+    {
+        if (empty($url)) {
+            throw new Exception("Parser::__constructor - 'no url'");
+        } else {
+            $this->url = $url;
+            $this->getContent();
+        };
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function setUrl($url)
+    {
+        if (empty($url)) {
+            return false;
+        } else {
+            $this->url = $url;
+            return true;
+        };
+    }
+
+    public function getContent()
+    {
+        $this->htmlContent = file_get_contents($this->url);
+        return true;
+    }
+
+    public function getInfo()
+    {
+        $rating = [];
+
+        if (preg_match("/<div class=\"table-group\">(.*?)<\/div>/is", $this->htmlContent, $tableBlock)) {
+            if (preg_match_all("/<td>(\d+?)<\/td>.*?<td class=\"table-link\">.*?<img src=\".*?\" width=\"\d+\" height=\"\d+\" title=\"(.*?)\".*?<td>(\d+?)<\/td>.*?<td>(\d+?)<\/td>.*?<td>(\d+?)<\/td>.*?<td>(\d+?)<\/td>/is", $tableBlock[1], $countries, PREG_SET_ORDER) !== false) {
+                foreach ($countries as $country) {
+                    array_push($rating, [
+                        'rank'   => (int)$country[1],
+                        'title'  => $country[2],
+                        'gold'   => (int)$country[3],
+                        'silver' => (int)$country[4],
+                        'bronze' => (int)$country[5],
+                        'total'  => (int)$country[6],
+                    ]);
+                }
+            }
+        };
+
+        return $rating;
+
+    }
+}
+
+
+$p = new Parser("http://news.sportbox.ru/paralympic/stats/reiting_197");
+
+echo json_encode($p->getInfo());
